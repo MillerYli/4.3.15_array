2c53fb3b9919a0fcab43ef1e92244a2e48cf06de
Yulia Miller <miller82.98@mail.ru>
2022-12-19 00:53:33 -0800

    Добавьте файлы проекта.

---
+++ 4.3.15 array/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace mynamespase
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            var arr = new int[] { 8, 9, 6, 0, 8, 4, 2 };
+
+            int count = 0;
+
+            foreach(int i in arr)
+            {
+                count++;
+            }
+            Console.WriteLine("Кол-во элементов в массиве: {0}", count);
+        }
+    }
+}
